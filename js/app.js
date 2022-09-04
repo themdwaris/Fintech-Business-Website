0c67bee41cf8f50f7a3ec6f27c9c8ecf0ec609ab
Mohammad Waris <mdtech9900@gmail.com>
2022-09-04 12:31:43 +0530

    Pusing a business website Fintech

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+function openMenu()
+{
+    document.getElementById('openMenu').style.left='0px';
+    // document.getElementById('open').style.display='none';
+    // document.getElementById('close').style.display='block';
+
+
+}
+function closeMenu()
+{
+    document.getElementById('openMenu').style.left='-280px';
+    // document.getElementById('open').style.display='block';
+    // document.getElementById('close').style.display='none';
+
+
+}
